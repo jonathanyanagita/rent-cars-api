cd7fa87344104dbf9958f19aecdfd21a8c37fc2e
Jonathan <jonathanyanagita@gmail.com>
2024-07-02 12:35:15 -0300

    Feat: UpdateRequest classes

--- src/main/java/br/gov/sp/fatec/domain/request/AluguelUpdateRequest.java
+++ src/main/java/br/gov/sp/fatec/domain/request/AluguelUpdateRequest.java
@@ -1,3 +1,7 @@
 package br.gov.sp.fatec.domain.request;
 
-public record AluguelUpdateRequest() {}
+import br.gov.sp.fatec.domain.enums.AluguelStatus;
+
+import java.util.Date;
+
+public record AluguelUpdateRequest(AluguelStatus status, Date dataFim, Double valor) {}
